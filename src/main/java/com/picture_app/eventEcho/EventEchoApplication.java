a48bc4ba95de196dddd2ec14d7d61156c4fbb4d9
Pawan V <pawan.vijayaraghavan98@gmail.com>
2025-06-06 00:50:12 +0530

    Initial Commit

---
+++ src/main/java/com/picture_app/eventEcho/EventEchoApplication.java
@@ -0,0 +1,13 @@
+package com.picture_app.eventEcho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventEchoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventEchoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/picture_app/eventEcho/EventEchoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picture_app.eventEcho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventEchoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
